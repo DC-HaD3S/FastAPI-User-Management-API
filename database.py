ccd2ac3acf767f84722a05b1f8c2c18f177ac263
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-06-05 17:38:52 +0530

    added all files

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = "postgresql+psycopg2://postgres:root@localhost:5432/fastapi"
+
+engine=create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,10 @@
+from pydantic import EmailStr, Field
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True, index=True)
+    username=Column(String(50),unique=True)
+    email = Column(String(255), unique=True, nullable=True)
+    password = Column(String(255), nullable=True) 
